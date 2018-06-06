290104a052b22dae6beb7502b71e7bd7053e95e9
colohr <colohr.code@gmail.com>
2018-06-06 09:02:18 -0500

    fixer.io api update
    Now requires access_key for using fixer.io to fetch current rates

--- package/rates.js
+++ package/rates.js
@@ -1,16 +1,34 @@
-const http = require('https')
-const source_locator = 'https://api.fixer.io/latest'
+const fxy = require('fxy')
+const query = require('qs')
+const source_locator = (protocol='http', parameters)=>`${protocol}://data.fixer.io/api/latest?${parameters}`
 const rates = {
 	update(date){ return this.date !== date },
 	get data(){ return 'data_rates' in this ? this.data_rates:require('./converts') },
-	set data(data){ return this.data_rates = data }
+	set data(data){ return this.data_rates = data },
+	get http(){ return require(this.protocol) },
+	get locator(){ return get_locator() },
+	get protocol(){ return this.options.license === 'paid' ? 'https':'http' }
 }
 
 //exports
 module.exports = get_rates
 
 //shared actions
-async function get_rates(){
+function get_locator(){
+	return source_locator(rates.protocol, query.stringify(rates.options.parameters))
+}
+
+function get_options(options){
+	if(!options){
+		const location = fxy.join(process.cwd(), 'app.json')
+		if(!fxy.exists(location)) throw new Error(`Missing API access: currency.order requires options for "fixer.io" to access exchange rates.  Add a fixer.io account info to the "api" object in an "app.json" file in the top-level directory of the project or provide an options object when accessing the rates module.`)
+		options = require(location).api['fixer.io']
+	}
+	return options
+}
+
+async function get_rates(options=null){
+	if('options' in rates !== true) rates.options = get_options(options)
 	if(!rates.loading && is_expired()){
 		if('date' in rates) rates.last = rates.date
 		rates.data = await load_rates()
@@ -19,13 +37,12 @@ async function get_rates(){
 	return rates
 }
 
-
 function load_rates(){
 	let data = ''
 	rates.loading = true
 	return new Promise((success,error)=>{
 		return process.nextTick(()=>{
-			return http.get( source_locator, response=>{
+			return rates.http.get(rates.locator, response=>{
 				const status = response.statusCode
 				const type = response.headers['content-type']
 				if(status !== 200) return error(new Error(`Request Failed.\n Status Code: ${status}`))
--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
-const {bundle,ChronologicalOrder} = require('../package/index')
-const api_key = 'AIzaSyAqsYk_afj-3fh6ALDD_hd64lSgCFdUIBk'
-const input = {
-	"name": "Armenia",
-	"coordinates": {
-		"latitude": 40.13475528,
-		"longitude": 45.01072318
+const currency = require('../package/index')
+const api_options = {
+	parameters:{
+		access_key: '3b42f19c72cde18f53aa57231dfca20f',
+		format:1
 	}
 }
+
+
 
 test().then(result=>{
 
@@ -15,7 +15,7 @@ test().then(result=>{
 })
 
 async function test(){
-	const timezone = await bundle.timezone(input,api_key)
-	console.dir(timezone,{depth:5,colors:true})
+	const api = await currency.rates(api_options)
+	console.dir(api,{depth:5,colors:true})
 }
 
